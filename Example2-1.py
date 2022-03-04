5aa63418a29f30c03029911765c8bcd8d596939e
Josh <jlane5261@gmail.com>
2022-03-03 18:59:52 -0600

    deleted .txt and added .py created initial constants

---
+++ Example2-1.py
@@ -0,0 +1,32 @@
+#!/usr/bin python3
+
+"""
+This example problem is simply to try and convey some of the equations this chapter introduces.
+Super easy plug and chug
+"""
+
+#Problem as stated:
+# A rocket projectile has the following characteristics:
+# Initial mass                               200 kg
+# Mass after rocket operation                130 kg
+# Payload, nonpropulsive structure, etc.     110 kg
+# Rocket operating duration                  3.0 seconds
+# Average specific impulse of propellant     240 seconds
+# 
+# Determine the following: 
+# Mass Ratio for the vehicle and propulsive unit
+# propellant mass fraction for the vehicle and propulsive unit
+# The effective exhaust velocity (c)
+# the Total Impulse (I_t)
+# 
+# If maximum acceleration cannot exceed 35 gs, does this flight exceed max g? 
+# 
+
+
+# Define constants
+
+m_o     = 200 #(kg)
+m_veh   = 130 #(kg)
+m_non   = 110 #(kg)
+dur     = 3 #(seconds)
+I_s     = 240 #(seconds)
