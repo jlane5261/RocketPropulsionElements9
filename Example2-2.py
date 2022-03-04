94f8504e1f319d5f94cf3850745920bf3f65e81d
Josh <jlane5261@gmail.com>
2022-03-03 21:40:58 -0600

    added example 2-2

---
+++ Example2-2.py
@@ -0,0 +1,20 @@
+#!/usr/bin python3
+
+'''
+The following measurements were made in a sea level test of a solid
+propellant rocket motor:
+
+Burn duration                           40 (seconds)
+Initial mass before test                1210 (kg)
+Mass of rocket motor after test         215 (kg)
+Average thrust                          62,250 (N)
+Chamber pressure                        7.00 (MPa)
+Nozzle exit pressure                    0.07 (MPa)
+Nozzle throat diameter                  0.0855 (m)
+Nozzle exit diameter                    0.2703 (m)
+
+
+Determine m_dot, v_2, c*, and c at sea level. Also, determine the pressure thrust and Isp at sea level, 1000 m, and 25000 m altitude.
+Assume an invariant thrust and mass flow rate and negligible short start and stop
+transients. 
+'''
